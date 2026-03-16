9cdac1744e264d715cf39334c27b414acc63ad8b
上坂蓮華 <129679783+kamisaka108@users.noreply.github.com>
2026-03-16 21:55:30 +0800

    Add import for style.css in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "./style.css";
 import { navItems, slides, cast, nfTags, nfHotLives, nfFixedFeeds, departments } from './data';
 import {
   Shield,Users,Lock,Search,ChevronRight,ChevronLeft,ChevronDown,FileText,
